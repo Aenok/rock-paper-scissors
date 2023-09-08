16182130e45ea6d7a5f695b3a8af0f0c003192ba
Ryan <ryan.stutz06@gmail.com>
2023-09-08 16:27:29 -0400

    Added HTML buttons for the options in the rps game, and made it count to 5 wins before announcing a winnner

--- scripts.js
+++ scripts.js
@@ -1,41 +1,81 @@
 let choiceArray = ["Rock", "Paper", "Scissors"];
-let counter = 0;
+let resultDiv = document.getElementById("results");
+let playerWins = 0;
+let compWins = 0;
 
 function getComputerChoice() {
     return Math.floor(Math.random()*3);
 }
 
 function playRound(playerSelection, computerSelection) {
+
+    resultDiv.innerText = "";
+
     if(playerSelection === computerSelection) {
-         console.log("Stalemate!");
+        resultDiv.innerText = "Stalemate!";
     } else if (playerSelection === 2 && computerSelection === 0 ){
-        console.log(`You Lose! ${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}!`);
+        compWins += 1;
+        resultDiv.innerText = `You Lose! ${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}!`;
     } else if (playerSelection === 0 && computerSelection === 2) {
-        counter += 1;
-        console.log(`You Win! ${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}!`);
+        playerWins += 1;
+        resultDiv.innerText = `You Win! ${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}!`;
     } else if (playerSelection > computerSelection){
-        counter += 1;
-        console.log(`You Win! ${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}!`); 
+        playerWins += 1;
+        resultDiv.innerText = `You Win! ${choiceArray[playerSelection]} beats ${choiceArray[computerSelection]}!`; 
     } else {
-        console.log(`You Lose! ${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}!`);
+        compWins += 1;
+        resultDiv.innerText = `You Lose! ${choiceArray[computerSelection]} beats ${choiceArray[playerSelection]}!`;
     }
 }
 
-function game() {
+function game(playerChoice) {
   
-    let playerChoice;
     let compChoice;
+    let buttons;
    
-    for (let i = 0; i < 5; i++) {
-        playerChoice = prompt("Choose from Rock, Paper, or Scissors");
-        let firstLetter = playerChoice.slice(0,1).toUpperCase();
-        playerChoice = choiceArray.indexOf(firstLetter + playerChoice.substr(1).toLowerCase());
-        compChoice = getComputerChoice();
 
-        playRound(playerChoice, compChoice);
+
+    // for (let i = 0; i < 5; i++) {
+    //     playerChoice = prompt("Choose from Rock, Paper, or Scissors");
+    //     let firstLetter = playerChoice.slice(0,1).toUpperCase();
+    //     playerChoice = choiceArray.indexOf(firstLetter + playerChoice.substr(1).toLowerCase());
+    compChoice = getComputerChoice();
+
+    playRound(playerChoice, compChoice);
         
+    // }
+
+
+
+    if(playerWins === 5) {
+        resultDiv.innerText = `You've won 5 times! You win everything~`;
     }
 
-    return `You won ${counter} times!`;
+    if(compWins === 5) {
+        resultDiv.innerText = `The computer won 5 times! You're a loser~`;
+    }
+
+    if (playerWins === 5 || compWins === 5) {
+        buttons = document.querySelectorAll("button");
+        for(let i = 0; i < buttons.length; i++) {
+            buttons[i].disabled = true;
+        }
+    }
     
 }
+
+const rockBtn = document.getElementById("rock");
+const paperBtn = document.getElementById("paper");
+const scissorBtn = document.getElementById("scissors");
+
+rockBtn.addEventListener("click", () => {
+    game(0);
+});
+
+paperBtn.addEventListener("click", () => {
+    game(1);
+})
+
+scissorBtn.addEventListener("click", () => {
+    game(2);
+})
